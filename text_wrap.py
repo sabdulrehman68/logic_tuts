c8c1254f0effe897c6f8678c2230c78079d0d450
sabdulrehman68 <sabdulrehman68@gmail.com>
2023-03-03 16:43:56 +0530

    text wrap program

---
+++ text_wrap.py
@@ -0,0 +1,20 @@
+import textwrap
+
+def wrap(string, max_width):
+    text = textwrap.fill(string,max_width)
+    return text
+
+if __name__ == '__main__':
+    string, max_width = input(), int(input())
+    result = wrap(string, max_width)
+    print(result)
+
+# To wrap the string like:
+#ABCDEFGHIJKLIMNOQRSTUVWXYZ to 
+# ABCD
+# EFGH
+# IJKL
+# IMNO
+# QRST
+# UVWX
+# YZ
